7f09692df04f23558929852b975986ff374cb46b
Pol Jansà Sierra <poljansa.developer@gmail.com>
2026-05-31 10:41:21 +0200

    Update profile image import in Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMobileAlt } from '@fortawesome/free-solid-svg-icons'
-import perfil from '../assets/perfil.png'
+import perfil from '../assets/perfilbo.png'
 
 const Sidebar = ({ active, setActive, lang, setLang, t }) => {
   const items = [
